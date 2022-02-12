c2c74997be14751b573e165e877df2be04d33e8f
Yuan Ker Pang <e0660595@u.nus.edu>
2022-02-12 19:29:32 +0800

    Complete part 1

--- Lab02/part1/autograder/grade.sh
+++ Lab02/part1/autograder/grade.sh
@@ -1,7 +1,49 @@
 #!/bin/bash
 
 # Check if we have enough arguments
-
+if [ $# != 1 ]; then
+  echo "Usage: ./grade.sh <filename>"
+else
+  gcc ref/*.c -o $1
+  for output in ./ref/*.out; do
+    if [[ -e $output ]]; then
+      rm $output
+    fi
+  done
+  >results.out
+  result="results.out"
+  echo "Test date and time: $(date +"%A, %d %B %Y, %X")" >> $result
+  echo "" >> $result
+  tasks=0
+  for i in ./ref/*.in; do
+    ./$1  < $i > $i.out
+    tasks=$((tasks + 1))
+  done
+  students=0
+  for dir in ./subs/*; do
+    students=$((students + 1))
+    dirname=$(basename $dir)
+    gcc ${dir}/*.c -o ${dir}/$1
+    if [[ $? -ne 0 ]]; then
+      echo "Directory $dirname has a compile error" >> $result
+      echo "Directory $dirname score 0/$tasks" >> $result
+    else
+     for i in ./ref/*.in; do
+       inputname=$(basename $i)
+       ${dir}/$1 < $i > ${dir}/$inputname.out
+     done
+     total=$tasks
+     for output in ./ref/*.out; do
+       outputname=$(basename $output)
+       cmp -s $output ${dir}/$outputname
+       total=$((total - $?))
+     done
+     echo "Directory $dirname score $total/$tasks" >> $result
+    fi
+  done
+  echo "" >> $result
+  echo "Processed $students files." >> $result
+fi
 # Delete temporary files
 
 # Compile the reference program
