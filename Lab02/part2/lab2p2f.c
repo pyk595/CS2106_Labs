f975e02d48800356d937e3d76e425c598e32d839
Ng Jun Kang <ng_jun_kang@hotmail.com>
2022-02-15 13:32:58 +0800

    Add Lab 2 Part 2

--- Lab02/part2/lab2p2f.c
+++ Lab02/part2/lab2p2f.c
@@ -6,15 +6,35 @@
 
 int main() {
 
-    printf("Be patient, the program will take around 7 seconds to run.\n");
-    printf("At the end you can do \"cat results.out\" to see the result.\n");
+  printf("Be patient, the program will take around 7 seconds to run.\n");
+  printf("At the end you can do \"cat results.out\" to see the result.\n");
 
-    //
-    // Add code here to pipe from ./slow 5 to ./talk and redirect
-    // output of ./talk to results.out
-    // I.e. your program should do the equivalent of ./slow 5 | talk > results.out
-    // WITHOUT using | and > from the shell.
-    //
+  //
+  // Add code here to pipe from ./slow 5 to ./talk and redirect
+  // output of ./talk to results.out
+  // I.e. your program should do the equivalent of ./slow 5 | talk > results.out
+  // WITHOUT using | and > from the shell.
+  //
+  int p[2];
+  int fp_out = open("./results.out", O_CREAT | O_WRONLY);
 
+  if(pipe(p) < 0)
+    perror("lab2p2f: ");
+
+  if(fork() != 0) {
+    close(p[0]);
+    dup2(p[1], STDOUT_FILENO);
+    execlp("./slow", "slow", "5", NULL);
+    close(p[1]);
+  } else {
+    close(p[1]);
+    dup2(p[0], STDIN_FILENO);
+    dup2(fp_out, STDOUT_FILENO);
+    execlp("./talk", "talk", (char *) 0);
+    close(p[0]);
+    close(fp_out);
+  }
+
+  return 0;
 }
 
