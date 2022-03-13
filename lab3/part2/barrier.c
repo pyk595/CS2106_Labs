a3f874d20113b6efc779efbf54e95955254a8ead
Yuan Ker Pang <e0660595@u.nus.edu>
2022-03-13 22:28:25 +0800

    Barrir first attempt

--- lab3/part2/barrier.c
+++ lab3/part2/barrier.c
@@ -1,13 +1,40 @@
 #include <stdio.h>
+#include <semaphore.h>
+#include <sys/shm.h>
+
+int nproc, count, shmid;
+sem_t *mutex, *barrier;
 
 void init_barrier(int numproc) {
+  nproc = numproc;
+  count = 0;
+  shmid = shmget(IPC_PRIVATE, 2 * sizeof(sem_t), IPC_CREAT | 0600);
+  mutex = (sem_t *) shmat(shmid, NULL, 0);
+  sem_init(mutex, 1, 1);
+  barrier = (sem_t *) shmat(shmid, NULL, 0);
+  sem_init(barrier, 1, 0);
 }
 
 void reach_barrier() {
+  sem_wait(mutex);
+  count++;
+  sem_post(mutex);
+  if (count == nproc) {
+    sem_post(barrier);
+  }
+  else {
+    sem_wait(barrier);
+    sem_post(barrier);
+  }
 }
 
 void destroy_barrier(int my_pid) {
     if(my_pid != 0) {
+      sem_destroy(barrier);
+      sem_destroy(mutex);
+      shmdt(mutex);
+      shmdt(barrier);
+      shmctl(shmid, IPC_RMID, 0);
         // Destroy the semaphores and detach
         // and free any shared memory. Notice
         // that we explicity check that it is
--- lab3/part2/barrier.h
+++ lab3/part2/barrier.h
@@ -1,3 +1,4 @@
+int nproc, count;
 void init_barrier(int);
 void reach_barrier();
 void destroy_barrier(int);
